3e6b5d5d83028a3d63cf7fed16d89e75546491fe
alexcutschall <alexander.cutschall@gmail.com>
2018-01-31 10:10:49 -0700

    Iteration 1 confirming that both capitals and numbers are translated correctly

--- test/translator_test.rb
+++ test/translator_test.rb
@@ -6,11 +6,20 @@ require 'pry'
 class MorseTranslatorTest < Minitest::Test
   def test_translator_class_exists
     translator = Translate.new
+
     assert_equal Translate, translator.class
   end
 
   def test_eng_to_morse_function
     translator = Translate.new
+
     assert_equal "......-...-..--- .-----.-..-..-..", translator.eng_to_morse("hello world")
+  end
+
+  def test_eng_to_morse_with_numbers_and_capitals
+    translator = Translate.new
+
+    assert_equal "......-...-..--- .-----.-..-..-..", translator.eng_to_morse("Hello World")
+    assert_equal "-......-.. .-.-.. ...-- ..........--....", translator.eng_to_morse("There are 3 ships")
   end
 end

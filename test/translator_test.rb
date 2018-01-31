416fd896fe906374f91b6c7333fce0fbd8d91704
alexcutschall <alexander.cutschall@gmail.com>
2018-01-31 09:25:55 -0700

    Adding the Translate class

--- test/translator_test.rb
+++ test/translator_test.rb
@@ -1,1 +1,10 @@
-# YOUR TESTS GO IN HERE!
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/translator.rb'
+
+class MorseTranslatorTest < Minitest::Test
+  def test_translator_class_exists
+    translator = Translate.new
+    assert_equal Translate, translator.class
+  end
+end
